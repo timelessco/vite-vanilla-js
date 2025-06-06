4f71fb51cb0dc340aa2fa71b9b9a42386cc31ddd
Navin Moorthy <navin007.a@gmail.com>
2025-06-06 22:37:32 +0530

    feat(tooling)!: updated to latest standards followed in other repos
    - Added .editorconfig for consistent coding styles.
    - Created .env.example for environment variable setup.
    - Introduced .gitattributes to manage line endings and file types.
    - Added .markdownlint-cli2.jsonc for markdown linting configuration.
    - Created .npmrc for PNPM settings.
    - Added .prettierignore to specify files to ignore for Prettier.
    - Updated cspell.json with new terms.
    - Modified package.json to include additional built dependencies.
    - Added prettier.config.cjs for Prettier configuration.
    - Introduced turbo.json for task management.
    - Created .github/labels.yml for issue labeling.
    - Added workflows for cleaning up PR caches and syncing labels.
    - Introduced commit-msg hook for commit message linting.

---
+++ prettier.config.cjs
@@ -0,0 +1,66 @@
+const plugins = [
+	"prettier-plugin-packagejson",
+	"prettier-plugin-curly",
+	"prettier-plugin-sh",
+	"@ianvs/prettier-plugin-sort-imports",
+	"prettier-plugin-tailwindcss",
+];
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+module.exports = {
+	importOrder: [
+		"",
+		"<BUILTIN_MODULES>",
+		"",
+		"",
+		"^(react/(.*)$)|^(react$)",
+		"^(next/(.*)$)|^(next$)",
+		"<THIRD_PARTY_MODULES>",
+		"",
+		"^types$",
+		"^@/lib/(.*)$",
+		"^@/ui/(.*)$",
+		"^@/components/(.*)$",
+		"^@/hooks/(.*)$",
+		"^@/stores/(.*)$",
+		"^@/icons/(.*)$",
+		"^@/utils/(.*)$",
+		"^@/styles/(.*)$",
+		"^@/app/(.*)$",
+		"^@(/.*)$",
+		"",
+		"^[../]",
+		"",
+		"^[./]",
+		"",
+		String.raw`^.+\.s?css$`,
+	],
+	importOrderParserPlugins: [
+		"typescript",
+		"jsx",
+		"decorators-legacy",
+		"importAssertions",
+	],
+	overrides: [
+		{ files: ".nvmrc", options: { parser: "yaml" } },
+		{ files: ".npmrc", options: { parser: "yaml" } },
+		{
+			files: ["*.jsonc"],
+			options: {
+				trailingComma: "none",
+			},
+		},
+		// Fixes [prettier-plugin-sort-imports]: import sorting aborted due to babel parsing error in markdown files
+		{
+			files: ["*.md"],
+			importOrder: undefined,
+			importOrderParserPlugins: undefined,
+			options: { plugins },
+		},
+	],
+	plugins,
+	tailwindFunctions: ["cn"],
+	tailwindStylesheet: "./src/css/global.css",
+	useTabs: true,
+};
